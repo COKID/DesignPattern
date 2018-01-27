4cf63bb4ccb1b0f4e13f0f02ee5298043067345a
COKID <Bourbon-Conan@outlook.com>
2018-01-27 22:26:10 +0800

    Build

---
+++ src/com/cokid/Prototype/UnderlinePen.java
@@ -0,0 +1,33 @@
+package com.cokid.Prototype;
+
+import com.cokid.Prototype.framework.Product;
+
+public class UnderlinePen implements Product {
+    private char ulchar;
+
+    public UnderlinePen(char ulchar) {
+        this.ulchar = ulchar;
+    }
+
+    @Override
+    public void use(String s) {
+        int lenght=s.getBytes().length;
+        System.out.println("\""+s+"\"");
+        System.out.print(" ");
+        for (int i=0;i<lenght;i++){
+            System.out.print(ulchar);
+        }
+        System.out.println("");
+    }
+
+    @Override
+    public Product createClone() {
+        Product p=null;
+        try{
+            p= (Product) clone();
+        } catch (CloneNotSupportedException e) {
+            e.printStackTrace();
+        }
+        return p;
+    }
+}
