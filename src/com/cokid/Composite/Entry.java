c00247694b0e4bddc5d87e4705891ea5e46485a3
COKID <Bourbon-Conan@outlook.com>
2018-01-31 12:28:56 +0800

    Decorator

---
+++ src/com/cokid/Composite/Entry.java
@@ -0,0 +1,19 @@
+package com.cokid.Composite;
+
+public abstract class Entry {
+    public abstract String getName();
+    public abstract int getSize();
+    protected abstract void printList(String prefix);
+
+    public Entry add(Entry entry)throws FileTreatmentException{
+        throw new FileTreatmentException();
+    }
+
+    public void printList(){
+        printList("");
+    }
+
+    public String toString(){
+        return getName()+"("+getSize()+")";
+    }
+}
---
+++ src/com/cokid/Composite/FileTreatmentException.java
@@ -0,0 +1,10 @@
+package com.cokid.Composite;
+
+public class FileTreatmentException extends RuntimeException {
+    public FileTreatmentException() {
+    }
+
+    public FileTreatmentException(String message) {
+        super(message);
+    }
+}
